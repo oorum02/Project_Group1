a73fcfb6256f6771d2ad994bb3741cb27ef82e60
Alissa Monova <alissamakesartthings@gmail.com>
2025-04-11 13:42:39 +0100

    Added Alissa's intro

--- Final_ReactApp/src/App.jsx
+++ Final_ReactApp/src/App.jsx
@@ -44,8 +44,8 @@ const teamMembers = [
     avatar: "",
 },
   {
-    name: " 🎟️ Gabriella",
-    films: "I love Spirited Away, The Blair Witch Project and Texas Chainsaw Massacre! One is very much not like the others 👻 ",
+    name: "📼 Alissa",
+    films: "Clueless for a feel good film, Annihilation or Dune for high quality sci-fi, and most Werner Herzog documentaries for a deadpan perspective.",
     avatar: "",
   }
 ];
