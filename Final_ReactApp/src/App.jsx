835f25e3bebc39ea6cfeb46dbbc750523c9fd8bd
Alissa Monova <alissamakesartthings@gmail.com>
2025-04-11 15:39:50 +0100

    updated App.jsx to incl. 1 intro per person

--- Final_ReactApp/src/App.jsx
+++ Final_ReactApp/src/App.jsx
@@ -33,11 +33,6 @@ const teamMembers = [
     avatar: "",
   }
   {
-    name: " 🎟️ Gabriella",
-    films: "I love Spirited Away, The Blair Witch Project and Texas Chainsaw Massacre! One is very much not like the others 👻 ",
-    avatar: "",
-  },
-  {
     name: " 📽️ Rima",
     films: "My favourite go to movie is Harry Potter and the Philosopher's Stone, also love the Fast and Furious franchise! I know, I know, but I love the action and the cars! 🚗💨",
     avatar: "",
