b5ee982b5382705f1a50b1166d50c31fbc162e5f
Alissa Monova <alissamakesartthings@gmail.com>
2025-04-11 15:38:02 +0100

    resolve merge conflict

--- Final_ReactApp/src/App.jsx
+++ Final_ReactApp/src/App.jsx
@@ -35,7 +35,6 @@ const teamMembers = [
   {
     name: " 🎟️ Gabriella",
     films: "I love Spirited Away, The Blair Witch Project and Texas Chainsaw Massacre! One is very much not like the others 👻 ",
-<<<<<<< HEAD
     avatar: "",
   },
   {
